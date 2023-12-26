734168df805b0e7ad796c31cc9ee4f5689289dbe
lacitadelle <jkomala99@gmail.com>
2023-12-26 16:12:44 +0700

    implemented fetching and status pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,50 @@
 import { useEffect, useState } from "react";
-import Product from "./components/Product"
-import axios from 'axios';
+import Product from "./components/Product";
+import axios from "axios";
 import "./App.css";
 const host = "http://localhost:4001";
 
 function App() {
+  const [status, setStatus] = useState("Loading...");
   const [productsDetail, setProductsDetail] = useState([]);
 
   function fetchProductsDetail() {
-    const data = axios.get(host + "/products").then(
-      res => {
-        setProductsDetail(res.data.data)
-      }
-    )
+    const data = axios
+      .get(host + "/products")
+      .then((res) => {
+        setProductsDetail(res.data.data);
+        setStatus("Success");
+      })
+      .catch((e) => {
+        console.error(e);
+        setStatus("Fetching Error...");
+      });
   }
 
-  useEffect(fetchProductsDetail, [])
+  useEffect(fetchProductsDetail, []);
 
   function deleteProduct(id) {
-    axios.delete(host + `/products/${id}`).then(
-      fetchProductsDetail()
-    )
-    
+    axios.delete(host + `/products/${id}`).then(fetchProductsDetail);
     return;
   }
 
-  const productsList = productsDetail.map(product => {
+  const productsList = productsDetail.map((product) => {
     return (
-      <Product data={product} deleteProduct={deleteProduct}/>
-    )
-  })
+      <Product key={product.id} data={product} deleteProduct={deleteProduct} />
+    );
+  });
 
-  return (
+  const statusPage = <h2>{status}</h2>;
+
+  return status === "Success" ? (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
-      <div className="product-list">
-        {productsList}
-      </div>
+      <div className="product-list">{productsList}</div>
     </div>
+  ) : (
+    statusPage
   );
 }
 
